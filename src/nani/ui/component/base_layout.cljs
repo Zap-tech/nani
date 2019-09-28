7f392bdc17ae1523ad02566c9aafe837c151f645
Benjamin Zaporzan <benzaporzan@gmail.com>
2019-09-28 17:37:18 -0400

    added nav header

--- src/nani/ui/component/base_layout.cljs
+++ src/nani/ui/component/base_layout.cljs
@@ -1,14 +1,16 @@
 (ns nani.ui.component.base-layout
   (:require
    [re-frame.core :as re]
-   [reagent.core :as r]))
+   [reagent.core :as r]
+
+   [nani.ui.component.navigation-header :refer [c-navigation-header]]))
 
 
 (defn c-base-layout
   [{:keys [container-opts] :as opts} & children]
   (let [opts (dissoc opts :container-opts)]
     [:div.base-layout opts
-     [:div.header "header"]
+     [:div.header [c-navigation-header]]
      [:div.main-margin
       [:div.main-container
        container-opts
---
+++ src/nani/ui/component/navigation_header.cljs
@@ -0,0 +1,17 @@
+(ns nani.ui.component.navigation-header
+  (:require
+   [reagent.core :as r]))
+
+
+(defn c-navigation-header
+  []
+  [:div.navigation-header
+   [:div.logo
+    [:span "Nani"]]
+   [:div.side
+    [:span "You are logged in as "]
+    [:span.name "bacon1989"]
+    [:div.sign-out
+     [:span "("]
+     [:div.button "sign out"]
+     [:span ")"]]]])
